16644a200b99ea11f20ea3f1cbb5fbb7fe7002dc
Angelo James R. Alag <alag.angelojames@gmail.com>
2020-09-26 13:12:29 +0800

    done portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,12 @@ let tl = gsap.timeline({
     scrollTrigger: {
         trigger: '.herosection',
         start: "center",
-        toggleActions: "restart none none none"
+        toggleActions: "restart none none none",
+        stagger: 0.2 
     }
 });
 
 tl.from(".about__cont", {y: 300, opacity: 0, duration: 1})
 tl.from(".feature__one", {y: 300, opacity: 0, duration: 1})
-tl.from(".project__content", {y: 300, opacity: 0, duration: 1})
-tl.from(".contact__me", {y: 300, opacity: 0, duration: 1})
 
 
