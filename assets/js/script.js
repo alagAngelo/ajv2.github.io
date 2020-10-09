389eca11037d13381f674c1c2543bfc09ae9a2ba
Angelo James R. Alag <alag.angelojames@gmail.com>
2020-10-09 20:37:19 +0800

    typo error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-let tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '.herosection',
-        start: "center",
-        toggleActions: "restart none none none",
-        stagger: 0.2 
-    }
-});
+// let tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.herosection',
+//         start: "center",
+//         toggleActions: "restart none none none",
+//         stagger: 0.2 
+//     }
+// });
 
-tl.from(".about__cont", {y: 300, opacity: 0, duration: 1})
-tl.from(".feature__one", {y: 300, opacity: 0, duration: 1})
+// tl.from(".about__cont", {y: 300, opacity: 0, duration: 1})
+// tl.from(".feature__one", {y: 300, opacity: 0, duration: 1})
+
+
+
+
+for (i = 0; i <= 100; i++){
+    if(i % 2 === 0 ){
+        console.log( i + "is even" );
+    }else{
+        console.log(i + "is odd");
+    }
+}
 
 
